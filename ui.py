b4bf08b5fbbd8fd90328f0b9a99ea31b241f2f3f
Ben Elster <benster1024@gmail.com>
2024-10-19 13:00:25 -0700

    api call to NWS

--- ui.py
+++ ui.py
@@ -1,1 +1,10 @@
-    printf("hello chat")
+import requests
+import json
+
+state = "NC"
+
+response = requests.get(f'https://api.weather.gov/alerts/active?area=CA').json()
+
+for x in response['features']:
+    print(x['properties']['description'])
+    
